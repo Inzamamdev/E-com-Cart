adc5ae31fb64d83f2599359366cbb5deff7da154
Inzamam Shaikh <92295764+456535688765@users.noreply.github.com>
2025-10-29 15:11:59 +0530

    server setup & clean up

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+
+app.listen(process.env.PORT | 3000, () => {
+  console.log("Server Started");
+});
